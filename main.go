225e063889019b7911f8cd6bc5385cce2b0d9d0c
Сельвестр Сталоневич <selvestr.stal@gmail.com>
2017-03-05 14:35:59 +0300

    hello world

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	log.Println("Client connected: " + r.RemoteAddr)
+	defer log.Println("Client disconnected: " + r.RemoteAddr)
+
+	_, err := w.Write([]byte("Hello World"))
+	if err != nil {
+		log.Println("Couldn`t write ", err.Error())
+		return
+	}
+}
 
 func main() {
-	fmt.Println("HEllo, World")
+	fmt.Println("Starting...")
+	http.HandleFunc("/", HelloServer)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
